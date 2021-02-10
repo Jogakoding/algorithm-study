4cea10c4301e4a017bae7cf3cd2a7f0b959351e1
minnczi <kmj1995kr@gmail.com>
2021-02-10 17:51:25 +0900

    question numbering fixed

--- SWEA/D2/1974/민지_1974.py
+++ SWEA/D2/1974/민지_1974.py
@@ -2,16 +2,31 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 T = int(input())
-wons = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
+def issudoku(S):
+    # row 검증
+    for row in S:
+        if len(row) != len(set(row)):
+            return 0
+    
+    # column 검증
+    for col in zip(*S):
+        if len(col) != len(set(col)):
+            return 0
+    
+    # square 검증 --> 가로 방향으로 이동하면서 square 하나씩 탐색
+    for i in range(0, 7, 3):
+        for j in range(0, 7, 3): 
+            square = S[i][j:j+3] + S[i+1][j:j+3] + S[i+2][j:j+3]
+            if len(square) != len(set(square)):
+                return 0
+    return 1
 
 for t in range(1, T+1):
-    money = int(input())
-    result = ['0'] * 8
-
-    for i in range(0, 8):
-        cnt, change = divmod(money, wons[i])
-        result[i] = str(cnt)
-        money = change
-    
-    print("#%d" % t)
-    print(" ".join(result))
+    # 입력 받는 부분
+    S = []
+    for _ in range(9):
+        row = list(map(int, input().split()))
+        S.append(row)
+    # issudoku 함수 호출
+    result = issudoku(S)
+    print("#%d %d" % (t, result))
