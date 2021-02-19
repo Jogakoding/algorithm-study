f0473fbb00f28de9bef8ed23a09a2416f5225895
minnczi <kmj1995kr@gmail.com>
2021-02-19 18:05:14 +0900

    10726

--- SWEA/D2/1961/민지_1961.py
+++ SWEA/D2/1961/민지_1961.py
@@ -3,7 +3,7 @@ sys.stdin = open('input.txt', 'r')
 
 T = int(input())
 
-def reverse_map(lst):
+def reverse_list(lst):
     return list(reversed(lst))
 
 
@@ -14,9 +14,9 @@ for t in range(1, T+1):
     for _ in range(N):
         square.append(list(map(str, input().split())))
     
-    square_90 = list(map(reverse_map, list(zip(*square))))
-    square_180 = list(map(reverse_map, list(zip(*square_90))))
-    square_270 = list(map(reverse_map, list(zip(*square_180))))
+    square_90 = list(map(reverse_list, list(zip(*square))))
+    square_180 = list(map(reverse_list, list(zip(*square_90))))
+    square_270 = list(map(reverse_list, list(zip(*square_180))))
 
     print('#%d' % t)
     for i in range(N):
